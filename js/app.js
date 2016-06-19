fbc64065c1ed6c0c744f70dee5a9507dd4aa66d0
dartess@mail.ru <dartess@mail.ru>
2016-06-19 15:33:44 +0400

    верстка мобильной версии

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+"use strict";
+
+ready(menuInit);
+ready(slidersInit);
+
 function ready(fn) {
     if (document.readyState != 'loading') {
         fn();
@@ -6,12 +11,56 @@ function ready(fn) {
     }
 }
 
-ready(function () {
-    var menu = document.querySelector('.main-nav');
-    var menuButton = document.querySelector('.js-main-nav');
-    menu.classList.remove('main-nav--no-js');
-    menuButton.classList.remove('page-header__menu-btn--hidden');
+function menuInit(){
+    var menuWrapper = document.querySelector('.page-header');
+    var menuButton = menuWrapper.querySelector('.page-header__menu-btn');
+    menuWrapper.classList.remove('page-header--no-js');
     menuButton.addEventListener('click', function () {
-        menu.classList.toggle('main-nav--close');
+        menuWrapper.classList.toggle('page-header--close');
     });
-});
+}
+
+function slidersInit(){
+    var sliders = document.querySelectorAll('.slider');
+    sliders.forEach(function (slider) {
+
+        slider.classList.remove('slider--no-js');
+
+        var sliderHeight = 0;
+        var sliderWrapper = slider.querySelector('.slider__wrapper');
+        var sliderDots = slider.querySelectorAll('.slider__dot-item');
+        var slides = slider.querySelectorAll('.slider__item');
+
+        var maybeStart = parseInt(slider.dataset.sliderStart);
+        var sliderInit = maybeStart && maybeStart <= slides.length ? --maybeStart : 0;
+
+        slides.forEach(function (item, i) {
+            item.style.position = 'absolute';
+            item.style.left = i * 100 + '%';
+            item.style.transform = 'translateX(-' + sliderInit * 100 + '%)';
+            var itemHeight = item.offsetHeight;
+            if (itemHeight > sliderHeight) {
+                sliderHeight = itemHeight;
+            }
+        });
+
+        sliderWrapper.style.height = sliderHeight + 'px';
+        sliderDots[sliderInit].classList.add('slider__dot-item--active');
+
+        sliderDots.forEach(function (dot, number) {
+            dot.addEventListener('click', function () {
+                slides.forEach(function (slide) {
+                    slide.style.transform = 'translateX(-' + number * 100 + '%)';
+                });
+                sliderDots.forEach(function (dotInner, numberInner) {
+                    if (number === numberInner) {
+                        dotInner.classList.add('slider__dot-item--active');
+                    } else {
+                        dotInner.classList.remove('slider__dot-item--active');
+                    }
+                })
+            })
+        })
+
+    });
+}
